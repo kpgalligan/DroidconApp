4fa3ae6ab057cd9c4eba2298014ece74e9735a49
apoorv <toidiu@gmail.com>
2015-11-09 18:10:28 -0500

    add task to get talk submission from network and db

--- app/src/main/java/co/touchlab/droidconandroid/tasks/GetTalkSubmissionTask.java
+++ app/src/main/java/co/touchlab/droidconandroid/tasks/GetTalkSubmissionTask.java
@@ -1,39 +1,42 @@
 package co.touchlab.droidconandroid.tasks;
 
 import android.content.Context;
-import android.util.Log;
-import co.touchlab.android.threading.eventbus.EventBusExt;
-import co.touchlab.android.threading.tasks.Task;
+
+import java.util.List;
+
+import co.touchlab.android.threading.tasks.TaskQueue;
+import co.touchlab.android.threading.tasks.persisted.PersistedTask;
 import co.touchlab.droidconandroid.data.DatabaseHelper;
-import co.touchlab.droidconandroid.data.Event;
 import co.touchlab.droidconandroid.data.TalkSubmission;
 import co.touchlab.droidconandroid.network.DataHelper;
 import co.touchlab.droidconandroid.network.TalkVotingWrapper;
 import co.touchlab.droidconandroid.network.VoteRequest;
+import co.touchlab.droidconandroid.tasks.persisted.PersistedTaskQueueFactory;
 import co.touchlab.squeaky.dao.Dao;
-
-import java.util.*;
 
 /**
  * Created by toidiu on 7/20/14.
  */
-public class GetTalkSubmissionTask extends Task {
-        public  List<Event> list ;
+public class GetTalkSubmissionTask extends PersistedTask
+{
+    public List<TalkSubmission> list;
+
+
+    public GetTalkSubmissionTask()
+    {
+    }
 
     @Override
     protected void run(Context context) throws Throwable
     {
-
-
         VoteRequest voteRequest = DataHelper.makeRequestAdapter(context).create(VoteRequest.class);
         List<TalkVotingWrapper> talkSubmission = voteRequest.getTalkSubmission();
-
-        Log.d("-----------------------ak", talkSubmission.get(0).talkSubmission.description);
-        List<TalkSubmission> talkSubmissions = TalkVotingWrapper.parseResp(talkSubmission);
+        list = TalkVotingWrapper.parseResp(talkSubmission);
 
 
         Dao<TalkSubmission, Long> dao = DatabaseHelper.getInstance(context).getTalkSubDao();
-        for(TalkSubmission t: talkSubmissions){
+        for(TalkSubmission t : list)
+        {
             dao.update(t);
         }
 
@@ -48,7 +51,7 @@ public class GetTalkSubmissionTask extends Task {
     @Override
     protected void onComplete(Context context)
     {
-            EventBusExt.getDefault().post(this);
+        TaskQueue.loadQueueDefault(context).execute(new GetDbTalkSubmissionTask(true));
     }
 
 }
