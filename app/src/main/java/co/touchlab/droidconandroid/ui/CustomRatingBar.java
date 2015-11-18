4da84fde79f39795e06bef6c0c845b61d6e901c7
apoorv <toidiu@gmail.com>
2015-11-18 16:49:54 -0500

    added custom rating bar

--- app/src/main/java/co/touchlab/droidconandroid/ui/CustomRatingBar.java
+++ app/src/main/java/co/touchlab/droidconandroid/ui/CustomRatingBar.java
@@ -15,12 +15,17 @@ import co.touchlab.droidconandroid.R;
  */
 public class CustomRatingBar extends View
 {
-
+    private final static int MAX_RATING = 5;
+    private float w24;
+    private float w16;
+    private float w40;
     private Paint fillPaint;
     private Paint emptyPaint;
 
-    public int rating = 0;
-    public int maxRating = 5;
+    //~=~=~=~=~=~=~=~=~=~=~=~=Fields
+    private int rating = 0;
+
+    private RatingChangeListener mChangeListener = null;
 
     public CustomRatingBar(Context context)
     {
@@ -39,6 +44,10 @@ public class CustomRatingBar extends View
         emptyPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
         emptyPaint.setStyle(Paint.Style.FILL);
         emptyPaint.setColor(res.getColor(R.color.vote_title_gray));
+
+        w24 = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 24, res.getDisplayMetrics());
+        w16 = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 16, res.getDisplayMetrics());
+        w40 = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 40, res.getDisplayMetrics());
     }
 
     public CustomRatingBar(Context context, AttributeSet attrs, int defStyleAttr)
@@ -50,35 +59,82 @@ public class CustomRatingBar extends View
     @Override
     protected void onDraw(Canvas canvas)
     {
-        Resources res = getResources();
-        float w24 = TypedValue
-                .applyDimension(TypedValue.COMPLEX_UNIT_DIP, 24, res.getDisplayMetrics());
-        float w16 = TypedValue
-                .applyDimension(TypedValue.COMPLEX_UNIT_DIP, 16, res.getDisplayMetrics());
-        float w40 = TypedValue
-                .applyDimension(TypedValue.COMPLEX_UNIT_DIP, 40, res.getDisplayMetrics());
-
         super.onDraw(canvas);
         //get width and divide by 5
         int height = canvas.getHeight();
-        int width = canvas.getWidth();
-        int i = width / 5;
-        int ihalf = i / 2;
 
         for(int x = 0; x < 5; x++)
         {
             Paint paint = (rating <= x)
-                    ? fillPaint
-                    : emptyPaint;
-            canvas.drawCircle(x * w40 + w16, height / 2, w16 - 10, paint);
+                    ? emptyPaint
+                    : fillPaint;
+            canvas.drawCircle(x * w40 + w24 / 2, height / 2, w24 / 2, paint);
         }
-
     }
 
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec)
+    {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+
+        int h = (int) Math.ceil(w24);
+        int w = (int) Math.ceil((w40 * 5) - w16);
+
+        setMeasuredDimension(w, h);
+    }
 
     @Override
     public boolean onTouchEvent(MotionEvent event)
     {
-        return super.onTouchEvent(event);
+        int division = getWidth() / MAX_RATING;
+        float x = event.getX();
+        float v = x / division;
+        setRating(v);
+        if(mChangeListener != null)
+        {
+            mChangeListener.onChange(rating);
+        }
+        return true;
     }
+
+    public void setRating(float v)
+    {
+        int ceil = (int) Math.ceil(v);
+        setRating(ceil);
+    }
+
+    public int getRating()
+    {
+
+        return rating;
+    }
+
+    public void setRating(int rate)
+    {
+        if(rate < 0)
+        {
+            rating = 0;
+        }
+        else if(rate > MAX_RATING)
+        {
+            rating = MAX_RATING;
+        }
+        else
+        {
+            rating = rate;
+        }
+        invalidate();
+    }
+
+    public void setChangeListener(RatingChangeListener changeListener)
+    {
+        mChangeListener = changeListener;
+    }
+
+    //~=~=~=~=~=~=~=~=~=~=~=~=Interface
+    public interface RatingChangeListener
+    {
+        void onChange(int rate);
+    }
+
 }
