8b4ffac847668ae2b1c21e38029556e8433f74c8
ramonaharrison <ramona.e.harrison@gmail.com>
2016-07-27 15:22:12 -0400

    Refactors profile screen to use scroll view

--- ios/ios/AboutViewController.swift
+++ ios/ios/AboutViewController.swift
@@ -11,21 +11,11 @@ import UIKit
 class AboutViewController: UIViewController {
 
     @IBOutlet weak var navBar: UINavigationBar!
-    @IBOutlet weak var stackView: UIStackView!
-    
-    //var scrollView: UIScrollView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         navBar.translucent = false
-        
-//        scrollView = UIScrollView(frame: view.bounds)
-//        scrollView.contentSize = stackView.bounds.size
-//        scrollView.autoresizingMask = [.FlexibleWidth, .FlexibleHeight]
-//        
-//        scrollView.addSubview(stackView)
-//        view.addSubview(scrollView)
     }
 
     override func didReceiveMemoryWarning() {
