66a19e7cdb7e81e835d1c073505c7fae22eed713
ramonaharrison <ramona.e.harrison@gmail.com>
2016-08-01 12:29:00 -0400

    Adds profile photo picker UI to iOS edit profile screen

--- ios/ios/EditProfileViewController.swift
+++ ios/ios/EditProfileViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class EditProfileViewController: UIViewController, DCPEditProfileHost {
+class EditProfileViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, DCPEditProfileHost {
     
     // MARK: Properties
 
@@ -33,6 +33,8 @@ class EditProfileViewController: UIViewController, DCPEditProfileHost {
     @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var contentView: UIView!
     
+    let imagePicker = UIImagePickerController()
+    
     var userDetailPresenter: DCPEditProfilePresenter!
     
     // MARK: Lifecycle events
@@ -46,10 +48,17 @@ class EditProfileViewController: UIViewController, DCPEditProfileHost {
         
         userDetailPresenter = DCPEditProfilePresenter(androidContentContext: DCPAppManager.getContext(), withDCPEditProfileHost: self)
         
+        imagePicker.delegate = self
+        
         navBar.translucent = false
         
         profileImageView.layer.cornerRadius = 21
         profileImageView.layer.masksToBounds = true
+        
+        // Setup tap gesture recognizer on the profile photo
+        let tapGesture = UITapGestureRecognizer(target: self, action: "profileImageTapped:")
+        profileImageView.addGestureRecognizer(tapGesture)
+        profileImageView.userInteractionEnabled = true
         
         phoneIcon.image = phoneIcon.image!.imageWithRenderingMode(UIImageRenderingMode.AlwaysTemplate)
         phoneIcon.tintColor = UIColor(red: 0/255.0, green: 65/255.0, blue: 163/255.0, alpha: 1.0)
@@ -79,6 +88,24 @@ class EditProfileViewController: UIViewController, DCPEditProfileHost {
     }
     
     // MARK: Actions
+    
+    func profileImageTapped(gesture: UIGestureRecognizer) {
+        imagePicker.allowsEditing = true
+        imagePicker.sourceType = .PhotoLibrary
+        
+        presentViewController(imagePicker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        if let pickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            profileImageView.contentMode = .ScaleAspectFill
+            profileImageView.image = pickedImage
+            
+            // TODO upload avatar image
+        }
+        
+        dismissViewControllerAnimated(true, completion: nil)
+    }
     
     @IBAction func saveProfile(sender: UIBarButtonItem) {
         userDetailPresenter.saveProfileWithNSString(nameField.text, withNSString: bioTextView.text, withNSString: companyField.text, withNSString: twitterField.text, withNSString: linkedInField.text, withNSString: websiteField.text, withNSString:facebookField.text, withNSString: phoneField.text, withNSString: emailField.text, withNSString: gplusField.text, withBoolean: !hideEmailSwitch.on)
