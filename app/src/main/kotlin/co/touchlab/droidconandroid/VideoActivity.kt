2c23f00d14b6172c36ffd85048ce5e563b9644a9
Sam Hill <samhill303@users.noreply.github.com>
2016-08-04 16:57:48 -0400

    Adds a placeholder video player activity for event streams.
    Currently plays a sample video. Still needs to be updated once the event model is updated with stream info.

---
+++ app/src/main/kotlin/co/touchlab/droidconandroid/VideoActivity.kt
@@ -0,0 +1,57 @@
+package co.touchlab.droidconandroid
+
+import android.content.res.Configuration
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.text.TextUtils
+import com.google.firebase.crash.FirebaseCrash
+import com.longtailvideo.jwplayer.events.listeners.VideoPlayerEvents
+import com.longtailvideo.jwplayer.media.playlists.PlaylistItem
+import kotlinx.android.synthetic.main.activity_video.*
+
+class VideoActivity : AppCompatActivity(), VideoPlayerEvents.OnFullscreenListener {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_video)
+        val link = intent.getStringExtra(EXTRA_STREAM_LINK)
+        if (!TextUtils.isEmpty(link)) {
+            val pi = PlaylistItem.Builder().file(link).title("BipBop").description("A video player testing video.").build()
+            jwplayer.load(pi)
+        } else {
+            FirebaseCrash.report(RuntimeException("Failed to load video link"))
+            finish()
+        }
+    }
+
+    override fun onResume() {
+        // Let JW Player know that the app has returned from the background
+        super.onResume()
+        jwplayer.onResume()
+    }
+
+    override fun onPause() {
+        // Let JW Player know that the app is going to the background
+        jwplayer.onPause()
+        super.onPause()
+    }
+
+    override fun onDestroy() {
+        // Let JW Player know that the app is being destroyed
+        jwplayer.onDestroy()
+        super.onDestroy()
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        // Set fullscreen when the device is rotated to landscape
+        jwplayer.setFullscreen(newConfig.orientation === Configuration.ORIENTATION_LANDSCAPE, true)
+        super.onConfigurationChanged(newConfig)
+    }
+
+    override fun onFullscreen(state: Boolean) {
+        if (state) {
+            actionBar.hide()
+        } else {
+            actionBar.show()
+        }
+    }
+}
