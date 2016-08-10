566434271e171a3051eca467cf3a13541104795f
ramonaharrison <ramona.e.harrison@gmail.com>
2016-08-10 14:06:29 -0400

    Refactors iOS event detail table rows to auto-size

--- ios/ios/EventTableViewCell.swift
+++ ios/ios/EventTableViewCell.swift
@@ -14,32 +14,28 @@ class EventTableViewCell: UITableViewCell {
     @IBOutlet weak var descriptionLabel : UILabel!
     @IBOutlet weak var timeInfoLabel : UILabel!
     @IBOutlet weak var liveStreamButton: UIButton!
+    @IBOutlet weak var liveStreamIcon: UIImageView!
     
     var event: DCDEvent!
     var eventDetailPresenter: DCPEventDetailPresenter!
     
     func loadInfo(title: String, description: String, track: String, time: String, event: DCDEvent, eventDetailPresenter: DCPEventDetailPresenter) {
+        
         self.event = event
         self.eventDetailPresenter = eventDetailPresenter
+        
         titleLabel.text = title
-        descriptionLabel.attributedText = formatHTMLString(description)
         timeInfoLabel.text = "Track " + track + ", " + time
+        descriptionLabel.attributedText = formatHTMLString(description)
         
         titleLabel.sizeToFit()
+        timeInfoLabel.sizeToFit()
         descriptionLabel.sizeToFit()
+        
+        liveStreamIcon.image = liveStreamIcon.image!.imageWithRenderingMode(UIImageRenderingMode.AlwaysTemplate)
+        liveStreamIcon.tintColor = UIColor(red: 0/255.0, green: 65/255.0, blue: 163/255.0, alpha: 1.0)
     }
-    
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-
+   
     func formatHTMLString(htmlString: String) -> NSAttributedString {
         let modifiedFont = NSString(format:"<span style=\"font: -apple-system-body; font-size: 12px\">%@</span>", htmlString) as String
         
